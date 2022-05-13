38da42014766cba97799d865c111b11d6e17550c
Geethanjaliks001 <100342019+Geethanjaliks001@users.noreply.github.com>
2022-05-13 16:41:41 +0530

    Add files via upload

--- 4b.sh
+++ 4b.sh
@@ -1,10 +1,17 @@
-if [ "$2" != " " ]
+if [ $# -eq 3 ]
 then
-cwd=`pwd`
-cd $2
-link=`ls -l $1 | tr -s " " | cut -d " "  -f2`
-cd $cwd
+if [ -f $1 ]
+then
+l =` cat $1 | wc -l`
+if [ $2 -gt $3 -a $2 -gt $1 a $3 -gt $2 ]
+then
+head -n $1 $3 | tail -n +$2
 else
-link=`ls -l $1 | tr -s " " | cut -d " " -f2`
+echo "enter valid argument"
 fi
-echo "Number of linkes of file $1: $link" 
+else
+echo "file doesnt exist"
+fi
+else
+echo "entre the nunber of argument"
+fi
