66119588a456c1c0e86888c42397570976405d9f
Geethanjaliks001 <100342019+Geethanjaliks001@users.noreply.github.com>
2022-05-20 15:30:20 +0530

    Update 4b.sh

--- 4b.sh
+++ 4b.sh
@@ -15,3 +15,29 @@ fi
 else
 echo "entre the nunber of argument"
 fi
+
+
+
+or
+
+
+if [ $# -ge 1 ]
+then
+   if [ $# -eq 2 ]
+   then
+	chd=$2
+   else
+	chd=`cd .`
+   fi
+   file=`find $chd -iname $1`
+   if [ `echo $file | wc -c` -gt 1 ]
+   then
+     number=`ls -inum $file | grep -o ^[0-9]*`
+     cd $chd
+     find $chd -inum $number
+   else
+     echo "No such file"
+  fi
+else
+echo "Enter argument"
+fi
